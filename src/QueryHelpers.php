b9c00023252124edf19ce0c773e923fdea61b7c2
Leo <losoviz@gmail.com>
2019-11-03 17:09:38 +0800

    Extracted duplicated code

--- src/QueryHelpers.php
+++ src/QueryHelpers.php
@@ -1,6 +1,8 @@
 <?php
 namespace PoP\FieldQuery;
 
+use PoP\QueryParsing\Facades\QueryParserFacade;
+
 class QueryHelpers
 {
     public static function listFieldArgsSymbolPositions(string $field): array
@@ -40,5 +42,19 @@ class QueryHelpers
     public static function getEmptyFieldArgs(): string
     {
         return QuerySyntax::SYMBOL_FIELDARGS_OPENING.QuerySyntax::SYMBOL_FIELDARGS_CLOSING;
+    }
+
+    public static function getFieldArgElements(?string $fieldArgsAsString): array
+    {
+        if ($fieldArgsAsString) {
+            // Remove the opening and closing brackets
+            $fieldArgsAsString = substr($fieldArgsAsString, strlen(QuerySyntax::SYMBOL_FIELDARGS_OPENING), strlen($fieldArgsAsString)-strlen(QuerySyntax::SYMBOL_FIELDARGS_OPENING)-strlen(QuerySyntax::SYMBOL_FIELDARGS_CLOSING));
+            // Remove the white spaces before and after
+            if ($fieldArgsAsString = trim($fieldArgsAsString)) {
+                $queryParser = QueryParserFacade::getInstance();
+                return $queryParser->splitElements($fieldArgsAsString, QuerySyntax::SYMBOL_FIELDARGS_ARGSEPARATOR, [QuerySyntax::SYMBOL_FIELDARGS_OPENING, QuerySyntax::SYMBOL_FIELDARGS_ARGVALUEARRAY_OPENING], [QuerySyntax::SYMBOL_FIELDARGS_CLOSING, QuerySyntax::SYMBOL_FIELDARGS_ARGVALUEARRAY_CLOSING], QuerySyntax::SYMBOL_FIELDARGS_ARGVALUESTRING_OPENING, QuerySyntax::SYMBOL_FIELDARGS_ARGVALUESTRING_CLOSING);
+            }
+        }
+        return [];
     }
 }
